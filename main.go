8608cb5cdc1d7b3218897308b6f3ce41bc6027f8
KrasnovDaniil1 <kd69931@gmail.com>
2022-06-22 23:19:06 +0200

    add

--- main.go
+++ main.go
@@ -32,10 +32,6 @@ type MemeoryStorage struct {
 	albums []album
 }
 
-type PostgresStorage struct {
-	db *sql.DB
-}
-
 func (s MemeoryStorage) Create(am album) album {
 	s.albums = append(s.albums, am)
 	return am
@@ -81,13 +77,67 @@ func NewMemoryStorage() MemeoryStorage {
 	return MemeoryStorage{albums: albums}
 }
 
+type PostgresStorage struct {
+	db *sql.DB
+}
+
+func (p PostgresStorage) CreateSchema() error {
+	_, err := p.db.Exec("Create TABLE if not EXISTS albums {ID char(16) primary key,Title char(128), Artist char(128), Price decimal}")
+	return err
+}
+
 func NewPostgresStorage() PostgresStorage {
-	connStr := "user=pqgotest dbname=pqgotest sslmode=verify-full"
+	connStr := "user=user dbname=db password=pass sslmode=verify-full"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return PostgresStorage{db: db}
+	storage := PostgresStorage{db: db}
+	err := storage.CreateSchema()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return storage
+}
+
+func (p PostgresStorage) Create(am album) album {
+	p.db.QueryRow("insert into albums(ID,Title,Artist,Price) values($1,$2,$3,$4)", am.ID, am.Title, am.Artist, am.Price)
+	return am
+}
+func (p PostgresStorage) ReadOne(id string) (album, error) {
+	var album album
+	p.db.QueryRow("select * from albums where id = $1", id).Scan(&album)
+	return album, nil
+	// for _, v := range s.albums {
+	// if v.ID == id {
+	// return v, nil
+	// }
+	// }
+	// return album{}, errors.New("not_found")
+}
+func (p PostgresStorage) Read() []album {
+	return s.albums
+}
+func (p PostgresStorage) Update(id string, newAlbum album) (album, error) {
+	for i, _ := range s.albums {
+		if s.albums[i].ID == id {
+			// c.BindJSON(&albums[i])
+			s.albums[i] = newAlbum
+			// c.IndentedJSON(http.StatusNoContent, albums[i])
+			return s.albums[i], nil
+		}
+	}
+	return album{}, errors.New("not_found")
+}
+func (s MemeoryStorage) Delete(id string) error {
+	for i, v := range s.albums {
+		if v.ID == id {
+			s.albums = append(s.albums[:i], s.albums[i+1:]...)
+			return nil
+		}
+	}
+	return errors.New("not_found")
+
 }
 
 func NewStorage() Storage {
